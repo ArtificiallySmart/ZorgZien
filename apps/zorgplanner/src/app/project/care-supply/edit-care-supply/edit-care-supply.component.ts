01e135dfaa542e4a40e1a6140771ad314fd84523
Bryan <bryanparcival@gmail.com>
2023-12-23 18:24:03 +0100

    chore: formControls ecs

--- apps/zorgplanner/src/app/project/care-supply/edit-care-supply/edit-care-supply.component.ts
+++ apps/zorgplanner/src/app/project/care-supply/edit-care-supply/edit-care-supply.component.ts
@@ -13,68 +13,126 @@ import {
   CareSupplyList,
   EditCareSupplyList,
 } from '../../../shared/interfaces/care-supply';
-import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
+import {
+  NgbAccordionModule,
+  NgbCollapseModule,
+} from '@ng-bootstrap/ng-bootstrap';
 import { zipcodeValidator } from '../../../shared/validators/zipcode.directive';
 import { areaZipcodeValidator } from '../../../shared/validators/area-zip-code.directive';
 import { CareSupplyService } from '../../services/care-supply.service';
+import { ColorPickerModule } from 'ngx-color-picker';
 
 @Component({
   selector: 'zorgplanner-edit-care-supply',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, NgbCollapseModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    NgbCollapseModule,
+    NgbAccordionModule,
+    ColorPickerModule,
+  ],
   templateUrl: './edit-care-supply.component.html',
   styleUrl: './edit-care-supply.component.scss',
 })
 export class EditCareSupplyComponent implements OnInit {
   @Input() careSupplyList!: CareSupplyList;
+
   careSupplyService = inject(CareSupplyService);
   careSupplyListForm!: FormGroup;
-  isCollapsed: boolean[] = [];
+
+  colorArray = [
+    'hsl(345,80%,50%)',
+    'hsl(128,50%,47%)',
+    'hsl(52,100%,55%)',
+    'hsl(227,66%,56%)',
+    'hsl(25,91%,58%)',
+    'hsl(286,71%,41%)',
+    'hsl(191,89%,61%)',
+    'hsl(303,86%,57%)',
+    'hsl(77,84%,60%)',
+    'hsl(338,86%,86%)',
+    'hsl(173,37%,44%)',
+    'hsl(268,100%,87%)',
+    'hsl(32,62%,37%)',
+    'hsl(55,100%,89%)',
+    'hsl(0,100%,25%)',
+    'hsl(138,100%,83%)',
+    'hsl(60,100%,25%)',
+    'hsl(30,100%,85%)',
+    'hsl(240,100%,23%)',
+  ];
+
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
-    this.careSupplyListForm = this.createCareSupplyListForm(
-      this.careSupplyList
-    );
-    this.isCollapsed = this.careSupplyList.careSupply.map(() => true);
+    this.initForm();
   }
 
-  createCareSupplyListForm(supplyList: CareSupplyList): FormGroup {
-    const FormGroup = this.fb.group({
-      title: [supplyList.title, Validators.required],
+  initForm() {
+    this.careSupplyListForm = this.fb.group({
+      id: [this.careSupplyList.id],
+      title: [this.careSupplyList.title, Validators.required],
       careSupply: this.fb.array([]),
     });
-
-    supplyList.careSupply.forEach((supplyEntry) => {
-      const supplyEntryForm = this.createCareSupplyEntryForm(supplyEntry);
-      (FormGroup.get('careSupply') as FormArray).push(supplyEntryForm);
-    });
-
-    return FormGroup;
+    this.careSupplyList.careSupply.forEach((supplyEntry) =>
+      this.createCareSupplyEntryForm(supplyEntry)
+    );
+    this.careSupplyListForm.disable();
   }
 
-  createCareSupplyEntryForm(supplyEntry: CareSupplyEntry): FormGroup {
+  get careSupply() {
+    return this.careSupplyListForm.get('careSupply') as FormArray;
+  }
+
+  cancelEdit() {
+    this.careSupplyListForm.reset();
+    this.careSupplyListForm.disable();
+  }
+
+  supplyById(index: number, supply: CareSupplyEntry) {
+    return supply.id;
+  }
+
+  zipCodeById(index: number) {
+    return index;
+  }
+
+  getInnerFormControl(outerIndex: number, innerIndex: number): FormControl {
+    return (
+      (this.careSupplyListForm.get('careSupply') as FormArray)
+        .at(outerIndex)
+        .get('areaPostalCodes') as FormArray
+    ).at(innerIndex) as FormControl;
+  }
+
+  createCareSupplyEntryForm(supplyEntry: CareSupplyEntry) {
     const entry = this.fb.group({
       name: [supplyEntry.name, Validators.required],
       amount: [supplyEntry.amount],
       color: [supplyEntry.color],
-      areaPostalCodes: this.fb.array([], [areaZipcodeValidator()]),
+      areaPostalCodes: this.fb.array(
+        [
+          ...(supplyEntry.areaPostalCodes ?? []).map(
+            (x) => new FormControl(x, [zipcodeValidator()])
+          ),
+        ],
+        [areaZipcodeValidator()]
+      ),
     });
-    if (supplyEntry.areaPostalCodes) {
-      supplyEntry.areaPostalCodes.forEach((postalCode) => {
-        (entry.get('areaPostalCodes') as FormArray).push(
-          this.fb.control(postalCode, [Validators.required, zipcodeValidator()])
-        );
-      });
-    }
-    return entry;
+    this.careSupply.push(entry);
   }
 
   deleteCareSupplyEntry(index: number) {
-    const careSupplyArray = this.careSupplyListForm.get(
-      'careSupply'
-    ) as FormArray;
-    careSupplyArray.removeAt(index);
+    this.careSupply.removeAt(index);
+  }
+  color(index: number) {
+    return this.careSupply.at(index).get('color')?.value;
+  }
+
+  changeColor(color: string, index: number) {
+    const careSupplyEntry = this.careSupply.at(index) as FormGroup;
+    careSupplyEntry.patchValue({ color: color });
   }
 
   onSubmit() {
@@ -95,25 +153,21 @@ export class EditCareSupplyComponent implements OnInit {
     this.careSupplyService.editCareSupplyList(editCareSupplyList);
   }
 
-  addPostalCode(value: string, index: number) {
-    const postalCode = new FormControl(value, [
+  addZipcode(el: HTMLInputElement, index: number) {
+    const value = el.value;
+    const zipcode = new FormControl(value, [
       Validators.required,
       zipcodeValidator(),
     ]);
-    const careSupplyArray = this.careSupplyListForm.get(
-      'careSupply'
-    ) as FormArray;
-    const careSupplyEntry = careSupplyArray.at(index) as FormGroup;
-    const postalCodes = careSupplyEntry.get('areaPostalCodes') as FormArray;
-    postalCodes.push(postalCode);
+    const careSupplyEntry = this.careSupply.at(index) as FormGroup;
+    const zipcodes = careSupplyEntry.get('areaPostalCodes') as FormArray;
+    zipcodes.push(zipcode);
+    el.value = '';
   }
 
-  deletePostalCode(index: number, postalCodeIndex: number) {
-    const careSupplyArray = this.careSupplyListForm.get(
-      'careSupply'
-    ) as FormArray;
-    const careSupplyEntry = careSupplyArray.at(index) as FormGroup;
+  deleteZipcode(index: number, zipcodeIndex: number) {
+    const careSupplyEntry = this.careSupply.at(index) as FormGroup;
     const postalCodes = careSupplyEntry.get('areaPostalCodes') as FormArray;
-    postalCodes.removeAt(postalCodeIndex);
+    postalCodes.removeAt(zipcodeIndex);
   }
 }
